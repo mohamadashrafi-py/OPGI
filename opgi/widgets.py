ba8c0677a573a139d9498865715a0d36437a3b73
mohamad <mo.mohamadashrafi1@gmail.com>
2025-08-18 11:04:55 +0330

    Add button widget to create simple button

--- opgi/widgets.py
+++ opgi/widgets.py
@@ -30,3 +30,42 @@ class Label(Widget):
         gl.glRasterPos2f(self.x, self.y)
         for char in self.text:
             glut.glutBitmapCharacter(glut.GLUT_BITMAP_HELVETICA_18, ord(char))
+
+
+class Button(Widget):
+    def __init__(self, text, x, y, width=100, height=40):
+        super().__init__(x, y, width, height)
+        self.text = text
+        self.color = (0.2, 0.5, 0.8)
+        self.pressed = False
+        self.on_click_callback = None
+
+    def on_click(self):
+        if self.on_click_callback:
+            self.on_click_callback()
+
+    def set_on_click(self, callback):
+        self.on_click_callback = callback
+
+    def draw(self):
+        # Draw button background
+        gl.glColor3f(*self.color)
+        gl.glBegin(gl.GL_QUADS)
+        gl.glVertex2f(self.x, self.y)
+        gl.glVertex2f(self.x + self.width, self.y)
+        gl.glVertex2f(self.x + self.width, self.y + self.height)
+        gl.glVertex2f(self.x, self.y + self.height)
+        gl.glEnd()
+
+        # Draw button text
+        text_width = sum(
+            glut.glutBitmapWidth(glut.GLUT_BITMAP_HELVETICA_18, ord(c))
+            for c in self.text
+        )
+        text_x = self.x + (self.width - text_width) // 2
+        text_y = self.y + self.height // 2 + 5
+
+        gl.glColor3f(1, 1, 1)
+        gl.glRasterPos2f(text_x, text_y)
+        for char in self.text:
+            glut.glutBitmapCharacter(glut.GLUT_BITMAP_HELVETICA_18, ord(char))
